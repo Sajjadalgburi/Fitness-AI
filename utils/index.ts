01923f33e3b203082591e724ea592ab24d33e1e4
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-22 11:28:51 -0700

    consant vars that wont be changed such as mood

---
+++ utils/index.ts
@@ -0,0 +1,7 @@
+export const moods = [
+  { mood: "Happy", emoji: "😊" },
+  { mood: "Sad", emoji: "😢" },
+  { mood: "Angry", emoji: "😡" },
+  { mood: "Surprised", emoji: "😲" },
+  { mood: "Confused", emoji: "😕" },
+];
