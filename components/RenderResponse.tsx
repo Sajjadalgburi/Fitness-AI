91d9e536cbc00e10a0a45c9a25f6a3396faead14
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-22 16:01:21 -0700

    improving the chat card

--- components/RenderResponse.tsx
+++ components/RenderResponse.tsx
@@ -1,5 +1,6 @@
 import { Message } from "ai";
 import React from "react";
+import Image from "next/image";
 
 interface RenderResponseProps {
   messages: Message[];
@@ -7,17 +8,39 @@ interface RenderResponseProps {
 
 const RenderResponse: React.FC<RenderResponseProps> = ({ messages }) => {
   return (
-    <div className="response">
-      {messages.map((m, index) => (
+    <div className="space-y-4">
+      {messages.map((m) => (
         <div
           key={m.id}
-          className={`flex items-center p-8 border-b-4 bg-slate-300 ${
-            m.role === "user" ? "user-chat" : "ai-chat"
+          className={`flex overflow-hidden rounded-xl shadow-xl w-fit  ${
+            m.role === "user" ? "bg-accent" : "bg-secondary"
           }`}
         >
-          <div className="flex m-8">
-            <p className="message">{m.content}</p>
-            {index < messages.length - 1 && <div className="horizontal-line" />}
+          <div
+            className={`flex items-start p-4 gap-4 ${
+              m.role === "user" ? "flex-row-reverse" : "flex-row justify-start"
+            }`}
+          >
+            <Image
+              src={m.role === "user" ? "/user.jpg" : "/ai-avatar.jpg"}
+              alt={m.role === "user" ? "User image" : "AI image"}
+              width={55}
+              height={55}
+              className="rounded-full"
+            />
+
+            <div
+              className={`flex flex-col max-w-sm ${
+                m.role === "user"
+                  ? "items-end text-right"
+                  : "items-start text-left"
+              }`}
+            >
+              <p className="text-base font-bold">
+                {m.role === "user" ? "You" : "AI"}
+              </p>
+              <p className="text-base font-medium">{m.content}</p>
+            </div>
           </div>
         </div>
       ))}
