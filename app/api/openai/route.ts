2bea5f85ac48888cfc1803aca85a39c5e3624c8e
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-22 15:09:18 -0700

    prompt engineered the AI and returned the response to front end

--- app/api/openai/route.ts
+++ app/api/openai/route.ts
@@ -3,25 +3,43 @@ import { streamText } from "ai";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
-  console.log("Messages:", messages[0].content); // Output - Messages: Hello
-
   try {
-    const model = openai("gpt-3.5-turbo"); // Choose a model based on your requirements
-    if (!model)
-      return NextResponse.json({ error: "Model not found" }, { status: 404 });
+    const { messages } = await req.json();
 
-    const results = streamText({
+    // Ensure that messages and content exist
+    if (!messages || !messages[0]?.content) {
+      return NextResponse.json(
+        { error: "No valid content in the request." },
+        { status: 400 }
+      );
+    }
+
+    console.log("Messages:", messages[0]?.content); // Log the content
+
+    const model = openai("gpt-3.5-turbo"); // Choose a model based on your requirements
+    if (!model) {
+      return NextResponse.json({ error: "Model not found" }, { status: 404 });
+    }
+
+    // Stream text from the AI model
+    const results = await streamText({
       model,
-      system: "Prompt Engnieering Here to improve AI response",
+      system:
+        "You are Sarah, a movie expert working for MoodFlix! " +
+        "Your role is to help users find movies based on their mood or a description of a movie they've seen. " +
+        "Upon receiving a user's input, first reassure them with a friendly and engaging response. " +
+        "Then, analyze their input and return a concise query that the MoodFlix application will use to call the TMDb API. " +
+        "Ensure your response is structured clearly and is easy to extract programmatically. " +
+        "Format your response exactly as follows: " +
+        "reassurance_message, query: keyword=[value], genre=[value], sort_by=[value]. Do not add desc." +
+        "Make sure there are no line breaks or extra spaces between elements in the response. " +
+        "Always use lowercase for keys and values, and separate query parameters with commas and spaces. ",
       prompt: messages[0].content,
     });
 
     return results.toDataStreamResponse();
-
-    // Catch error if any occurs
   } catch (error) {
-    console.log(error);
+    console.error("Error processing request:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
