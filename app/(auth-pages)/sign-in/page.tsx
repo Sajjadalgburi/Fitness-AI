400481b753c8db837b4265d3b65a99f50064fd70
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-23 11:36:07 -0700

    refactor sign-in page: update layout and change labels for consistency

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -8,12 +8,12 @@ import Link from "next/link";
 export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
-    <form className="flex-1 flex flex-col min-w-64">
-      <h1 className="text-2xl font-medium">Sign in</h1>
+    <form className="flex flex-col min-w-64 md:min-w-80 mx-auto p-7 rounded-lg shadow-2xl">
+      <h1 className="sm:text-4xl font-2xl font-bold">Login</h1>
       <p className="text-sm text-foreground">
         Don't have an account?{" "}
-        <Link className="text-foreground font-medium underline" href="/sign-up">
-          Sign up
+        <Link className="text-primary font-medium underline" href="/sign-up">
+          Register
         </Link>
       </p>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
@@ -35,7 +35,7 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
           required
         />
         <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
+          Login
         </SubmitButton>
         <FormMessage message={searchParams} />
       </div>
