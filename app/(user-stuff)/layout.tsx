4c90e3e785af4ab9c9515815fbb5ffb473641aa5
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-25 14:50:41 -0700

    Add gradient background to user layout

--- app/(user-stuff)/layout.tsx
+++ app/(user-stuff)/layout.tsx
@@ -3,5 +3,9 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <div className="bg-gradient-to-br from-indigo-900 via-purple-900 to-pink-800 text-white">
+      {children}
+    </div>
+  );
 }
