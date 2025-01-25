30b32cb9daf34c3285ce687467976b9c9877b651
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-25 14:13:07 -0700

    Refactor Navbar to use useUser hook for authentication state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,33 +1,10 @@
 import Link from "next/link";
 import React from "react";
-import { createClient } from "@/utils/supabase/client";
-import { useEffect, useState } from "react";
-import { Session } from "@supabase/supabase-js";
 import { signOutAction } from "@/app/actions";
-
-/**
- * Make the navbar smaller !
- */
+import { useUser } from "@/hooks/index";
 
 const Navbar = () => {
-  const [session, setSession] = useState<Session | null>(null);
-  const supabase = createClient();
-
-  useEffect(() => {
-    const fetchsession = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-
-      if (!session) {
-        return;
-      } else {
-        setSession(session);
-      }
-    };
-
-    fetchsession();
-  }, [supabase]);
+  const { user } = useUser();
 
   return (
     <nav className="shadow-lg mt-4 rounded-xl flex justify-between items-center px-7 py-2 max-w-4xl mx-auto fixed top-0 left-0 right-0 bg-primary-content z-10">
@@ -36,7 +13,7 @@ const Navbar = () => {
       </h1>
 
       <div className="flex items-center space-x-4">
-        {session ? (
+        {user !== null ? (
           <>
             <button className="btn" onClick={signOutAction}>
               Logout
