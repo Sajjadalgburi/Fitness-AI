69659a929b65dd35b9843f322680d00c2a77d5f9
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-25 13:37:18 -0700

    Configure ESLint to target specific directories during production builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    // ignoreDuringBuilds: true, // Ignore ESLint errors during production builds
+  },
 };
 
 export default nextConfig;
