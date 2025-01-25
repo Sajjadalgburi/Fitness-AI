e201725e562a1ec868f68fccbfa96ff7fa469bb7
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-25 14:49:51 -0700

    Refactor UserFooter component with improved styling and hover effects

--- components/user-footer.tsx
+++ components/user-footer.tsx
@@ -1,23 +1,24 @@
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Home, User } from "lucide-react";
 
 const UserFooter = () => {
   return (
-    <footer className="flex flex-col gap-3 justify-around items-center w-full max-w-xs bg-white shadow-xl rounded-lg p-2 overflow-hidden sm:mt-9">
-      <div className="flex gap-x-20">
-        <Link className="flex flex-col items-center gap-1" href="/">
-          Home
-          <Home />
-        </Link>
-        <Link className="flex flex-col items-center gap-1" href="/account">
-          Account
-          <User />
-        </Link>
-      </div>
-
-      <span>Fitness AI &copy; {new Date().getFullYear()}</span>
+    <footer className="flex gap-3 justify-around items-center w-full max-w-xs  text-warm-beige overflow-hidden mt-2 mx-auto">
+      <Link
+        className="flex flex-col items-center gap-1 text-gray-300 hover:scale-105 transition-all duration-200"
+        href="/"
+      >
+        Home
+        <Home />
+      </Link>
+      <Link
+        className="flex flex-col items-center gap-1 text-gray-300 hover:scale-105  transition-all "
+        href="/account"
+      >
+        Account
+        <User />
+      </Link>
     </footer>
   );
 };
