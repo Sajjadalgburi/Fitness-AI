bfe1f25dba0147ca68cd6922ee91fb38154251b9
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-25 13:37:13 -0700

    Add TypeScript interfaces for workout questionnaire and fitness profile management

---
+++ interface/index.ts
@@ -0,0 +1,43 @@
+import { FormEvent } from "react";
+
+export interface QCardProps {
+  handleSubmit: (e: React.FormEvent) => void;
+  age: string;
+  setAge: (value: string) => void;
+  weight: string;
+  setWeight: (value: string) => void;
+  gender: string;
+  setGender: (value: string) => void;
+  energyLevel: number;
+  setEnergyLevel: (value: number) => void;
+  fitnessGoal: string;
+  setFitnessGoal: (value: string) => void;
+  availabilityTime: string;
+  setAvailabilityTime: (value: string) => void;
+  preferredWorkout: string;
+  setPreferredWorkout: (value: string) => void;
+  availableEquipment: string[];
+  setAvailableEquipment: (value: string[]) => void;
+  isLoading: boolean;
+  error: string | null;
+}
+
+export interface WorkoutInfo {
+  id?: number;
+  age: string;
+  weight: string;
+  gender: string;
+  energyLevel: number;
+  fitnessGoal: string;
+  availabilityTime: string;
+  preferredWorkout: string;
+}
+
+export interface FitnessQuestionnaireFormProps {
+  setGender: (value: string) => void;
+  setEnergyLevel: (value: number) => void;
+  setFitnessGoal: (value: string) => void;
+  setAvailabilityTime: (value: string) => void;
+  setPreferredWorkout: (value: string) => void;
+  setAvailableEquipment: (value: string[]) => void;
+}
