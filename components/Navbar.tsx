8219d10619f10663a0903896ac718b36068921a5
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-27 09:50:42 -0700

    Improve Navbar responsiveness and styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,30 +7,31 @@ const Navbar = () => {
   const { user } = useUser();
 
   return (
-    <nav className="shadow-lg mt-4 rounded-xl flex justify-between items-center px-7 py-2 max-w-4xl mx-auto fixed top-0 left-0 right-0 bg-primary-content z-10">
-      <h1 className="text-2xl font-bold">
+    <nav className="shadow-lg mx-6 md:mt-4 rounded-xl flex justify-between items-center px-7 py-2 max-w-4xl md:mx-auto md:fixed top-0 left-0 right-0 bg-white md:z-10">
+      <h1 className="text-2xl font-bold text-black">
         <Link href="/">Fitness AI 🏋️</Link>
       </h1>
 
       <div className="flex items-center space-x-4">
         {user !== null ? (
           <>
-            <button className="btn" onClick={signOutAction}>
+            <button
+              className="btn btn-primary btn-sm md:btn-md"
+              onClick={signOutAction}
+            >
               Logout
             </button>
-            <button className="btn font-semibold btn-primary">
-              <Link href={"/account"}>Account</Link>
-            </button>
-            <button className="btn font-semibold btn-secondary">
+
+            <button className="btn font-semibold btn-secondary btn-sm md:btn-md">
               <Link href={"/workout"}>AI Chat</Link>
             </button>
           </>
         ) : (
           <>
-            <button className="btn font-semibold btn-primary">
+            <button className="btn font-semibold btn-primary btn-sm md:btn-md">
               <Link href={"/sign-in"}>Login</Link>
             </button>
-            <button className="btn font-semibold btn-secondary">
+            <button className="btn font-semibold btn-secondary btn-sm md:btn-md">
               <Link href={"/sign-up"}>Register</Link>
             </button>
           </>
