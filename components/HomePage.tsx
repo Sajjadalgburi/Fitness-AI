c9112b7c6ff41b712d1b216f3757852200860341
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-27 16:06:52 -0700

    Improve loading button spinner alignment in HomePage

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -341,7 +341,7 @@ const HomePage = () => {
                   className="btn btn-accent rounded-2xl md:w-1/2 sm:text-lg"
                 >
                   {isLoading ? (
-                    <span>
+                    <span className="flex items-center">
                       <svg
                         className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
                         xmlns="http://www.w3.org/2000/svg"
