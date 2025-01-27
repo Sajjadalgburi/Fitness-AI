9fb5cb92756c3dd67d67f9bfdcb2c0210c5540ab
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2025-01-27 13:50:38 -0700

    Simplify UserFooter to single home link with hover effect

--- components/user-footer.tsx
+++ components/user-footer.tsx
@@ -1,25 +1,17 @@
 import Link from "next/link";
 import React from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Home, User } from "lucide-react";
 
 const UserFooter = () => {
   return (
-    <footer className="flex gap-3 justify-around items-center w-full max-w-xs  text-warm-beige overflow-hidden mt-2 mx-auto">
-      <Link
-        className="flex flex-col items-center gap-1 text-gray-300 hover:scale-105 transition-all duration-200"
-        href="/"
-      >
-        Home
-        <Home />
-      </Link>
-      <Link
-        className="flex flex-col items-center gap-1 text-gray-300 hover:scale-105  transition-all "
-        href="/account"
-      >
-        Account
-        <User />
-      </Link>
-    </footer>
+    <Link
+      className="flex mt-2 flex-col items-center text-gray-300 hover:scale-105 transition-all duration-200"
+      href="/"
+    >
+      Home
+      <Home />
+    </Link>
   );
 };
 
